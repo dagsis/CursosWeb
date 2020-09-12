f822bfc459b48360031a08d9041e8137e799fe40
Carlos DAgostino <carlos_dagostino@hotmail.com>
2020-09-12 19:32:32 -0300

    Usuario Crear 1

---
+++ Aplicacion/Seguridad/Registrar.cs
@@ -0,0 +1,99 @@
+﻿
+using Aplicacion.Contratos;
+using Aplicacion.ManejadorError;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Seguridad
+{
+    public class Registrar
+    {
+        public class Ejecuta : IRequest<UsuarioData>
+        {
+            public string Nombre { get; set; }
+
+            public string Apellidos { get; set; }
+
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+
+            public string Username { get; set; }
+        }
+
+        public class EjecutaValidacion : AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidacion()
+            {
+                RuleFor(x => x.Nombre).NotEmpty();
+                RuleFor(x => x.Apellidos).NotEmpty();
+                RuleFor(x => x.Username).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, UsuarioData>
+        {
+            private readonly CursosOnLineContext context;
+            private readonly UserManager<Usuario> userManager;
+            private readonly IJwtGenerador jwtGenerador;
+
+            public Manejador(CursosOnLineContext context, UserManager<Usuario> userManager, IJwtGenerador jwtGenerador)
+            {
+                this.context = context;
+                this.userManager = userManager;
+                this.jwtGenerador = jwtGenerador;
+            }
+
+
+           
+            public async  Task<UsuarioData> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var existe = await this.context.Users.Where(x => x.Email == request.Email).AnyAsync();
+                if (existe)
+                {
+                    throw new ManejadorExepcion(HttpStatusCode.BadRequest, new { mensaje = "El email ingresado ya existe" });
+                }
+
+                var existeUserName = await this.context.Users.Where(x => x.UserName == request.Username).AnyAsync();
+                if (existe)
+                {
+                    throw new ManejadorExepcion(HttpStatusCode.BadRequest, new { mensaje = "El usuario ingresado ya existe" });
+                }
+
+                var usuario = new Usuario
+                {
+                    NombreCompleto = request.Nombre + " " + request.Apellidos,
+                    Email = request.Email,
+                    UserName = request.Username
+                };
+
+                var resultado = await this.userManager.CreateAsync(usuario, request.Password);
+                if (resultado.Succeeded)
+                {
+                    return new UsuarioData { 
+                        NombreCompleto = usuario.NombreCompleto,
+                        Token = this.jwtGenerador.CrearToken(usuario),
+                        UserName = usuario.UserName
+                    };
+                }
+
+                throw new ManejadorExepcion(HttpStatusCode.InternalServerError, new { mensaje = "No se pudo insertar los cambios" });
+
+            }
+        }
+    }
+}
